14c291246ae6f6c9d436dd3967b09f270267b9dd
Yoon-orn Chin <dylanchin123@gmail.com>
2021-03-11 08:54:53 -0800

    fixed lint error

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import calculator
+
+
+class TestCalculator:
+
+    def test_add(self):
+        assert 5 == calculator.add(1, 4)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(5, 3)
